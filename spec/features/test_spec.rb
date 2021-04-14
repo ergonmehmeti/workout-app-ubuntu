053e4fa733ddd199d8137adf90b4b4f60ee7aee2
ergonmehmeti <mehmeti.ergon@gmail.com>
2021-04-14 18:12:45 +0200

    Home page added

--- spec/features/test_spec.rb
+++ spec/features/test_spec.rb
@@ -4,6 +4,9 @@ RSpec.feature 'Test spec' do
 
   scenario 'visit home page' do
     visit '/'
-    expect(page).to have_content('Yay! You’re on Rails!')
+
+    expect(page).to have_link('Home')
+    expect(page).to have_link('Athletes Den')
+    expect(page).to have_content('Workout Lounge!')
   end
 end
